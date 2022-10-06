631d2be3da7c62759c02d0eb2deb0e4259fa776c
Goulart <goulart@cora.com.br>
2022-10-06 09:59:22 -0300

    Initial draft of proposal

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,10 @@
 package main
 
 import (
-	"context"
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/labstack/echo/v4"
-	"github.com/legoulart/poc-go/internal/port"
-	"github.com/legoulart/poc-go/primary/http"
-	"github.com/legoulart/poc-go/secondary/mongodb"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/legoulart/poc-go/config/di"
 )
 
 func main() {
-	e := echo.New()
-
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(""))
-	database := client.Database("poc")
-	session, err := client.StartSession()
-
-	if err != nil {
-		panic(err)
-	}
-
-	rep := mongodb.NewMongoDbRepository(database.Collection("users"), database.Collection("outbox"))
-	dap := mongodb.NewMongoDbAdapter(rep, session)
-
-	u := port.New(dap)
-
-	http.NewController(*e.Group(http.Path), u)
-
-	e.Logger.Fatal(e.Start(":1323"))
+	di.Setup()
 }
